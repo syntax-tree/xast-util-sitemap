0999c0ab819ac7938367e0e95d35798441778798
Titus Wormer <tituswormer@gmail.com>
2023-07-18 16:43:16 +0200

    Change to use `exports`

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 import assert from 'node:assert/strict'
 import test from 'node:test'
-import {sitemap} from './index.js'
+import {sitemap} from 'xast-util-sitemap'
 
 test('sitemap', async function (t) {
   await t.test('should expose the public api', async function () {
-    assert.deepEqual(Object.keys(await import('./index.js')).sort(), [
+    assert.deepEqual(Object.keys(await import('xast-util-sitemap')).sort(), [
       'sitemap'
     ])
   })
